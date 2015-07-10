0b3a8abfeaf22f7bff63d0450674623a57ade71e
Lauren <lauren.eastridge@formidablelabs.com>
2015-07-10 11:28:50 -0700

    updates boilerplate package.json

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var chalk = require('chalk');
 var yeoman = require('yeoman-generator');
+var beautify = require('js-beautify');
 
 var ReactComponentGenerator = yeoman.generators.Base.extend({
 
@@ -16,11 +17,22 @@ var ReactComponentGenerator = yeoman.generators.Base.extend({
       type: 'input',
       name: 'inputName',
       message: 'Please name your component',
-    }];
+    }, {
+      type: 'input',
+      name: 'author',
+      message: 'Please enter an author name'
+    }, {
+      type: 'input',
+      name: 'ghUser',
+      message: 'What github username / organization should this package be published under?',
+    },];
 
     this.prompt(prompts, function (props) {
       _.extend(this, props);
+      this.componentName = _.capitalize(_.camelCase(this.inputName));
       this.projectName = _.kebabCase(_.deburr(this.inputName));
+      this.repo = _.kebabCase(_.deburr(this.ghUser));
+      this.git = 'https://github.com/' + this.repo + '/' + this.projectName;
       this.destinationRoot(this.projectName);
       done();
     }.bind(this));
@@ -28,14 +40,27 @@ var ReactComponentGenerator = yeoman.generators.Base.extend({
 
   getBoilerplate: function () {
     this.remote(
-      "formidablelabs",
-      "formidable-react-component-boilerplate",
+      'formidablelabs',
+      'formidable-react-component-boilerplate',
       function (err, remote) {
         remote.directory('.', '.');
-        this.npmInstall();
-      }.bind(this),
-      true // removes the cached remote forcing it to download it fresh each time.
+      }.bind(this)
     );
+  },
+
+  updateJSON: function () {
+    var jsonFile = JSON.parse(this.read(this.destinationRoot() + '/package.json'));
+    jsonFile.name = this.projectName;
+    jsonFile.repository.url = this.git + '.git';
+    jsonFile.author = this.author;
+    jsonFile.bugs.url = this.git + "/issues";
+    jsonFile.homepage = this.git;
+    var updatedJSON = beautify(JSON.stringify(jsonFile), {indent_size: 2});
+    this.write(this.destinationRoot() + '/package.json', updatedJSON);
+  },
+
+  install: function() {
+    this.npmInstall();
   },
 
   end: function() {
